855f6c112a324730e8f92f279d14fa72087c99c8
Mar1Art1S <Oleg7471@outlook.com>
2024-09-13 17:01:13 +0300

    1

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,7 +14,7 @@ class User extends Authenticatable implements FilamentUser
     use HasFactory, Notifiable;
     public function canAccessPanel(Panel $panel): bool
     {
-        return str_ends_with($this->email, '@yourdomain.com') && $this->hasVerifiedEmail();
+        return str_ends_with($this->email, '@gmail.com') && $this->hasVerifiedEmail();
     }
     /**
      * The attributes that are mass assignable.
