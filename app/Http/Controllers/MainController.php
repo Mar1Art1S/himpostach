37a2d3142a43ef5b445c499a7eaa3358c50f30d9
Mar1Art1S <Oleg7471@outlook.com>
2024-09-14 19:02:42 +0300

    1

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -19,7 +19,7 @@ class MainController extends Controller
 
     public function index()
     {
-        $articles = Article::where('active', true)->latest('published_at')->paginate(4);
+        $articles = Article::where('active', true)->latest('published')->paginate(4);
         return view('article.index', compact('articles'));
         // $categories = ArticleCategory::where('active', true)->get();
         // return view('article.index', compact('categories'));
