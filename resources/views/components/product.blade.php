c03b667a649dfcb0439da11ea37f8d292f605fd5
Mar1Art1S <Oleg7471@outlook.com>
2024-09-30 11:29:26 +0300

    3

--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -71,7 +71,7 @@
                             </div>
                             <div class="relative w-auto">
                                 <div class="">
-
+                                    {!! $product->tds_content !!}
                                 </div>
                             </div>
                         </div>
