1b7d29545f500d141a428045f152cad051999f43
Mar1Art1S <Oleg7471@outlook.com>
2025-01-02 10:33:26 +0200

    search and product error

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -89,25 +89,22 @@
                                     <div x-data="{ modalOpen: false }" @keydown.escape.window="modalOpen = false"
                                          class="relative z-50 w-auto h-auto">
 
-                                        <!-- Кнопка відкриття модального вікна -->
                                         <button @click="modalOpen=true"
-                                                class="inline-block w-[100px] text-sm text-white text-center font-r500 hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-white disabled:opacity-75">
+                                                class="inline-block w-[100px]  text-sm text-white text-center font-r500 hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-white disabled:opacity-75">
                                             {{ __('search.search_technological_map') }}
                                         </button>
 
-                                        <!-- Модальне вікно -->
                                         <template x-teleport="body">
                                             <div x-show="modalOpen"
                                                  class="fixed top-0 left-0 z-[99] flex items-center justify-center w-screen h-screen"
                                                  x-cloak>
-                                                <!-- Фонове затемнення -->
                                                 <div x-show="modalOpen" x-transition:enter="ease-out duration-300"
-                                                     x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
-                                                     x-transition:leave="ease-in duration-300" x-transition:leave-start="opacity-100"
+                                                     x-transition:enter-start="opacity-0"
+                                                     x-transition:enter-end="opacity-100"
+                                                     x-transition:leave="ease-in duration-300"
+                                                     x-transition:leave-start="opacity-100"
                                                      x-transition:leave-end="opacity-0" @click="modalOpen=false"
                                                      class="absolute inset-0 w-full h-full bg-black bg-opacity-40"></div>
-
-                                                <!-- Вікно контенту -->
                                                 <div x-show="modalOpen" x-trap.inert.noscroll="modalOpen"
                                                      x-transition:enter="ease-out duration-300"
                                                      x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
@@ -116,20 +113,19 @@
                                                      x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
                                                      x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                                                      class="relative w-[860px] h-[700px] py-6 bg-white px-7 sm:max-w-lg sm:rounded-lg overflow-y-auto">
-
-                                                    <!-- Кнопка закриття -->
                                                     <button @click="modalOpen=false"
                                                             class="absolute top-0 right-0 flex items-center justify-center w-8 h-8 mt-5 mr-5 text-gray-600 rounded-full hover:text-gray-800 hover:bg-gray-50">
                                                         <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg"
-                                                             fill="none" viewBox="0 0 24 24"
-                                                             stroke-width="1.5" stroke="currentColor">
+                                                             fill="none"
+                                                             viewBox="0 0 24 24" stroke-width="1.5"
+                                                             stroke="currentColor">
                                                             <path stroke-linecap="round" stroke-linejoin="round"
                                                                   d="M6 18L18 6M6 6l12 12"/>
                                                         </svg>
                                                     </button>
-
-                                                    <!-- Контент модального вікна -->
-                                                    <div class="relative w-full h-full overflow-y-auto">
+                                                </div>
+                                                <div class="relative w-auto">
+                                                    <div class="">
                                                         {!! $product->tehcarta_content !!}
                                                     </div>
                                                 </div>
@@ -137,7 +133,6 @@
                                         </template>
                                     </div>
                                 @endif
-
                             </div>
                         </div>
 
@@ -204,52 +199,38 @@
                                     </div>
                                 @endif
 
-                                @if(!empty($product->tehcarta_content))
-                                    <div x-data="{ modalOpen: false }" @keydown.escape.window="modalOpen = false" class="relative z-50 w-auto h-auto">
-
-                                        <!-- Кнопка відкриття модального вікна -->
+                                @if($product->tehcarta_content)
+                                    <div x-data="{ modalOpen: false }" @keydown.escape.window="modalOpen = false"
+                                         class="relative z-50 w-auto h-auto">
                                         <button @click="modalOpen=true"
                                                 class="inline-block w-[100px] text-sm text-white font-r500 hover:bg-transparent hover:text-white focus:outline-none focus:ring active:text-white disabled:opacity-75">
                                             {{ __('search.search_technological_map') }}
                                         </button>
-
-                                        <!-- Модальне вікно -->
                                         <template x-teleport="body">
-                                            <div x-show="modalOpen" class="fixed inset-0 z-[99] flex items-center justify-center w-screen h-screen"
+                                            <div x-show="modalOpen"
+                                                 class="fixed top-0 left-0 z-[99] flex items-center justify-center w-screen h-screen"
                                                  x-cloak>
-                                                <!-- Фонове затемнення -->
                                                 <div x-show="modalOpen" @click="modalOpen=false"
                                                      class="absolute inset-0 w-full h-full bg-black bg-opacity-40"></div>
-
-                                                <!-- Контейнер модального вікна -->
                                                 <div x-show="modalOpen" x-trap.inert.noscroll="modalOpen"
-                                                     x-transition:enter="ease-out duration-300"
-                                                     x-transition:enter-start="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-                                                     x-transition:enter-end="opacity-100 translate-y-0 sm:scale-100"
-                                                     x-transition:leave="ease-in duration-200"
-                                                     x-transition:leave-start="opacity-100 translate-y-0 sm:scale-100"
-                                                     x-transition:leave-end="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
                                                      class="relative w-[860px] h-[700px] py-6 bg-white px-7 sm:max-w-lg sm:rounded-lg overflow-y-auto">
-
-                                                    <!-- Кнопка закриття -->
                                                     <button @click="modalOpen=false"
                                                             class="absolute top-0 right-0 w-8 h-8 mt-5 mr-5 text-gray-600 hover:text-gray-800 hover:bg-gray-50">
-                                                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                                        <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg"
+                                                             fill="none" viewBox="0 0 24 24"
                                                              stroke-width="1.5" stroke="currentColor">
-                                                            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12"/>
+                                                            <path stroke-linecap="round" stroke-linejoin="round"
+                                                                  d="M6 18L18 6M6 6l12 12"/>
                                                         </svg>
                                                     </button>
-
-                                                    <!-- Контент модального вікна -->
-                                                    <div class="relative w-full h-full overflow-y-auto">
-                                                        {!! $product->tehcarta_content !!}
+                                                    <div class="relative">
+                                                        <div>{!! $product->tehcarta_content !!}</div>
                                                     </div>
                                                 </div>
                                             </div>
                                         </template>
                                     </div>
                                 @endif
-
                             </div>
                         </div>
                     </div>
