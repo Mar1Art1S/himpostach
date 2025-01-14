6a01b4c021d6922fc0bcbab6f16761ae8ab56005
Mar1Art1S <Oleg7471@outlook.com>
2025-01-14 14:57:54 +0200

    name Blog

--- resources/views/components/decor/card.blade.php
+++ resources/views/components/decor/card.blade.php
@@ -1,4 +1,4 @@
-<div class="flex justify-center items-center ">
+<div class="flex justify-center items-center">
     <div
         class="grid grid-cols-4 grid-rows-1 gap-2 justify-start items-center tg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1">
         {{ $slot }}
